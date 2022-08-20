a697c7b1cce45781fbdcda8df17c2cca6d9d51f8
meganmcmahon <103215686+meganmcmahon@users.noreply.github.com>
2022-08-19 18:55:48 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,14 @@ MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors 
 lm(vehicle_length ~ vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg, MechaCar_mpg)
 
 summary(lm(vehicle_length ~ vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg, MechaCar_mpg))
+
+# Deliverable 2
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+library(tidyverse)
+
+total_summary <- suspension_coil  %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups = 'keep')
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups = 'keep')
+
