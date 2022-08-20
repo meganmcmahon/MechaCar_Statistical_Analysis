23edc0d40ed876e83bc7338e0b7fa1deab198ab1
meganmcmahon <103215686+meganmcmahon@users.noreply.github.com>
2022-08-20 09:35:58 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,18 @@ total_summary <- suspension_coil  %>% summarize(mean(PSI), median(PSI), var(PSI)
 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups = 'keep')
 
+# Deliverable 3
+
+t.test(suspension_coil$PSI,mu=mean(total_summary$`mean(PSI)`))
+
+# Lot 1
+
+t.test(total_summary,mu=mean(suspension_coil$PSI),subset=lot_summary$Lot1)
+
+# Lot 2
+
+t.test(total_summary,mu=mean(suspension_coil$PSI),subset=lot_summary$Lot2)
+
+# Lot 3
+
+t.test(total_summary,mu=mean(suspension_coil$PSI),subset=lot_summary$Lot3)
